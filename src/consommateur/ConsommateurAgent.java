6b460d97a70e0ce1e98c310104894a0a6398620d
Cubertin <Cubertin@franck>
2016-12-15 23:55:00 +0100

    Premier Comit del'histoire

---
+++ src/consommateur/ConsommateurAgent.java
@@ -0,0 +1,11 @@
+package consommateur;
+
+import jade.core.Agent;
+
+public class ConsommateurAgent extends Agent {
+	@Override
+	protected void setup() {
+		
+		System.out.println("Je suis le client qui veut achéter le livre !!");
+	}
+}
---
+++ src/consommateur/ConsommateurContainer.java
@@ -0,0 +1,27 @@
+package consommateur;
+
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+import jade.wrapper.ControllerException;
+
+public class ConsommateurContainer {
+
+	public static void main(String[] args) {
+		
+		try {
+			Runtime runtime = Runtime.instance();
+			ProfileImpl impl = new ProfileImpl(false);
+			impl.setParameter(ProfileImpl.MAIN_HOST, "localhost");
+			AgentContainer agentContainer = runtime.createAgentContainer(impl);
+			AgentController agentController = agentContainer.createNewAgent("Consommateur", "consommateur.ConsommateurAgent", null);
+			agentController.start();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
